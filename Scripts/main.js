4b2e8e1cf8fe0fd52fdd3e863a296229f0866716
tebogomonamodi08 <150713484+tebogomonamodi08@users.noreply.github.com>
2024-05-28 07:45:03 +0200

    Update on About page

--- Scripts/main.js
+++ Scripts/main.js
@@ -101,3 +101,42 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+/**********About Us on reveal */
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+  document.addEventListener('DOMContentLoaded', function() {
+    const dropdowns = document.querySelectorAll('.dropdown');
+  
+    dropdowns.forEach(dropdown => {
+      dropdown.addEventListener('click', function(event) {
+        event.stopPropagation(); // Prevent the default behavior of the link click
+        
+        // Toggle the dropdown content visibility
+        const content = this.querySelector('.dropdown-content');
+        content.classList.toggle('show');
+      });
+  
+      // Close the dropdown menu if the user clicks outside of it
+      document.addEventListener('click', function(event) {
+        if (!dropdown.contains(event.target)) {
+          dropdown.querySelector('.dropdown-content').classList.remove('show');
+        }
+      });
+    });
+  });
+  
+  
